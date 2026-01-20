0edd5867bfa56e3d724f56f945682ff8f3196a1c
Andrea-Ligorio <andreorio14@gmail.com>
2026-01-20 13:16:15 +0100

    inizio_creazione_accesso

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 document.querySelectorAll(".user_name").forEach(element => {element.innerText = "Andrea_Lig";});
 document.getElementById("squat").innerText = "160";
 
-stat = getElementById("select_rank");
 
 
-function showRank(value){
-    IPF = 25;
-    gl = 45;
-    total = 500;
-
+var IPF = 25;
+var gl = 45;
+var total = 500;
+var rank = total;
+document.getElementById("rank").innerText = rank;
+function setRank(value){
     switch(value){
-    case "TOTAL":
-        rank = total;
-        break;
-    case "IPF":
-        rank = IPF;
-        break;
-    case "GL":
-        rank = gl;
-        break;
+        case "TOTAL":
+            rank = total;
+            break;
+        case "IPF":
+            rank = IPF;
+            break;
+        case "GL":
+            rank = gl;
+            break;
     }
     document.getElementById("rank").innerText = rank;
 }
+
+
+
 
 
